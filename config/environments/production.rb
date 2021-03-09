f4674cce72ecc01af1ba533dea76144e2336b455
河合彰紘 <rakko1124@gmail.com>
2021-03-09 11:16:47 +0900

    [fix] production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = true
   config.assets.initialize_on_precompile=false
 

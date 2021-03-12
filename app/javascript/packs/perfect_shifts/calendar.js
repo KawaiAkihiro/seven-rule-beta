b1bba0aa08bd690eedded9198177ce1b7e626368
河合彰紘 <rakko1124@gmail.com>
2021-03-13 05:51:50 +0900

    [perfect_fix] 全戻し

--- app/javascript/packs/perfect_shifts/calendar.js
+++ app/javascript/packs/perfect_shifts/calendar.js
@@ -64,21 +64,55 @@ document.addEventListener('turbolinks:load', function() {
 
         eventClick: function(info){
             var id = info.event.id
-            $.ajax({
-                type: "GET",
-                url:  "/perfect_shifts/change",
-                data: { shift_id : id },
-                datatype: "html",
-            }).done(function(res){
-            
-                $('.modal-body').html(res)
-                $('#modal').fadeIn();
-            }).fail(function (result) {
-                // 失敗処理
-                alert("failed");
-            });
+            if(info.el.style.backgroundColor == "yellow"){
+                $.ajax({
+                    type: "GET",
+                    url:  "/perfect_shifts/fill",
+                    data: { shift_id : id },
+                    datatype: "html",
+                }).done(function(res){
+                
+                    $('.modal-body').html(res)
+                    $('#modal').fadeIn();
+                }).fail(function (result) {
+                    // 失敗処理
+                    alert("failed");
+                });
+            }else if(info.el.style.backgroundColor == "white"){
+                $.ajax({
+                    type: "GET",
+                    url:  "/perfect_shifts/change",
+                    data: { shift_id : id },
+                    datatype: "html",
+                }).done(function(res){
+                
+                    $('.modal-body').html(res)
+                    $('#modal').fadeIn();
+                }).fail(function (result) {
+                    // 失敗処理
+                    alert("failed");
+                });
+            }
             
         },
+
+        // eventClick: function(info){
+        //     var id = info.event.id
+        //     $.ajax({
+        //         type: "GET",
+        //         url:  "/perfect_shifts/change",
+        //         data: { shift_id : id },
+        //         datatype: "html",
+        //     }).done(function(res){
+            
+        //         $('.modal-body').html(res)
+        //         $('#modal').fadeIn();
+        //     }).fail(function (result) {
+        //         // 失敗処理
+        //         alert("failed");
+        //     });
+            
+        // },
 
 
         eventClassNames: function(arg){

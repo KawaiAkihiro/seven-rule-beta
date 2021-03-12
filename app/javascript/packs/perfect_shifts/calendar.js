54cb98d9100bfe8dae5c5d5574ff8a757271a84b
河合彰紘 <rakko1124@gmail.com>
2021-03-13 04:45:56 +0900

    [perfect_fix] perfect_shifts  / calendar jsの中身を temporary_shiftsと同じにしてみる

--- app/javascript/packs/perfect_shifts/calendar.js
+++ app/javascript/packs/perfect_shifts/calendar.js
@@ -1,15 +1,15 @@
 import { Calendar, whenTransitionDone } from '@fullcalendar/core';
 import interactionPlugin from '@fullcalendar/interaction';
 import weekGridPlugin from '@fullcalendar/timegrid'
+//import dayGridPlugin from '@fullcalendar/daygrid'
 import googleCalendarApi from '@fullcalendar/google-calendar'
 
 document.addEventListener('turbolinks:load', function() {
-
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new Calendar(calendarEl, {
         plugins: [ weekGridPlugin, interactionPlugin, googleCalendarApi ],
-        events: '/perfect_shifts.json',
+        events: '/temporary_shifts.json',
         googleCalendarApiKey: 'AIzaSyBJgxvPtAdElMF6qlcqWqIwFludRmesnOI',
         eventSources : [
             {
@@ -18,22 +18,16 @@ document.addEventListener('turbolinks:load', function() {
               color:"#FF8C00"
             }
         ],
-        slotDuration: "01:00:00" ,
         locale: 'ja',
         timeZone: 'Asia/Tokyo',
         scrollTime: '07:00:00',
-        dayCellContent: function(e) {
-            e.dayNumberText = e.dayNumberText.replace('時', '');
-
-        },
-        expandRows: true,
-        stickyHeaderDates: true,
-        headerToolbar: {
-            left: '',
-            center: 'title',
-            right: 'today prev,next' 
-        },
+        slotDuration: "01:00:00" ,
         firstDay: 1,
+        headerToolbar: {
+            start: '',
+            center: 'title',
+            end: 'today prev,next' 
+        },
         buttonText: {
             today: '今日'
         }, 
@@ -43,11 +37,27 @@ document.addEventListener('turbolinks:load', function() {
             const year  = info.date.getFullYear();
             const month = (info.date.getMonth() + 1);
             const day   = info.date.getDate();
+            const hour  = (info.date.getHours());
+
+            var ja_hour = 0
+
+            if(hour < 9){
+                ja_hour = hour + 15;
+            }else{
+                ja_hour = hour - 9;
+            }
+
+            var str_hour = ""
+
+            if(ja_hour < 10){
+                str_hour = "0" + ja_hour
+            }else
+                str_hour = ja_hour
 
             if(info.allDay){
                 $.ajax({
                     type: 'GET',
-                    url:  '/perfect_shifts/new_plan',
+                    url:  '/temporary_shifts/new_plan',
                 }).done(function (res) {
                     //イベント登録用のhtmlを作成
                     $('.modal-body').html(res);
@@ -62,40 +72,47 @@ document.addEventListener('turbolinks:load', function() {
                     // 失敗処理
                     alert("failed");
                 });
-            }
-        },        
+            }else{
+                $.ajax({
+                    type: 'GET',
+                    url:  '/temporary_shifts/new_shift',
+                }).done(function (res) {
+                    //イベント登録用のhtmlを作成
+                    $('.modal-body').html(res);
+                    
+                    $('#individual_shift_start_1i').val(year);
+                    $('#individual_shift_start_2i').val(month);
+                    
+                    if (str_hour >= 15){
+                        $('#individual_shift_start_3i').val(day-1);
+                    }else{
+                        $('#individual_shift_start_3i').val(day);
+                    }
+                    $('#individual_shift_start_4i').val(str_hour);
+                
+                    $('#modal').fadeIn();
+                    // 成功処理
+                }).fail(function (result) {
+                    // 失敗処理
+                    alert("failed");
+                });
+            }        
+        },
         eventClick: function(info){
             var id = info.event.id
-            if(info.el.style.backgroundColor == "yellow"){
-                $.ajax({
-                    type: "GET",
-                    url:  "/perfect_shifts/fill",
-                    data: { shift_id : id },
-                    datatype: "html",
-                }).done(function(res){
-                
-                    $('.modal-body').html(res)
-                    $('#modal').fadeIn();
-                }).fail(function (result) {
-                    // 失敗処理
-                    alert("failed");
-                });
-            }else if(info.el.style.backgroundColor == "white"){
-                $.ajax({
-                    type: "GET",
-                    url:  "/perfect_shifts/change",
-                    data: { shift_id : id },
-                    datatype: "html",
-                }).done(function(res){
-                
-                    $('.modal-body').html(res)
-                    $('#modal').fadeIn();
-                }).fail(function (result) {
-                    // 失敗処理
-                    alert("failed");
-                });
-            }
+            $.ajax({
+                type: "GET",
+                url:  "/temporary_shifts/delete",
+                data: { shift_id : id },
+                datatype: "html",
+            }).done(function(res){
             
+            $('.modal-body').html(res)
+            $('#modal').fadeIn();
+            }).fail(function (result) {
+                // 失敗処理
+                alert("failed");
+            });
         },
         eventClassNames: function(arg){
             if(arg.event.allDay){
@@ -117,7 +134,23 @@ $(function(){
     $('button.plan').click(function(){
         $.ajax({
             type: 'GET',
-            url:  '/perfect_shifts/new_plan',
+            url:  '/temporary_shifts/new_plan',
+        }).done(function (res) {
+            //イベント登録用のhtmlを作成
+            $('.modal-body').html(res);
+        
+            $('#modal').fadeIn();
+            // 成功処理
+        }).fail(function (result) {
+            // 失敗処理
+            alert("failed");
+        });
+    });
+
+    $('button.shift').click(function(){
+        $.ajax({
+            type: 'GET',
+            url:  '/temporary_shifts/new_shift',
         }).done(function (res) {
             //イベント登録用のhtmlを作成
             $('.modal-body').html(res);
@@ -130,4 +163,3 @@ $(function(){
         });
     });
 });
-
